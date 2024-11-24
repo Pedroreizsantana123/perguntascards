f51e24d0ed77f1bb38aef1bd956a18e83678ffc0
Pedroreizsantana123 <00001093434211sp@al.educacao.sp.gov.br>
2024-11-24 09:25:38 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+criaCartao(
+    'Comida',
+    'O que é o tomate',
+    'uma fruta usada em saladas'
+)
+/* Pedro.Reis.2c.10/11/2024
+*/
+criaCartao(
+    'Futebol',
+    'O que é uma carretilha?',
+    'carretilha é um drible no futebol.'
+)
+
+criaCartao(
+    'Brincadeira',
+    'O que é o jokepo?',
+    'Uma brincadeira onde as mãos sao usadas'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
